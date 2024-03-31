c4f39509e6074afa08c4780ef29550bac93c0021
Dylan-Kline <klinedylan46@gmail.com>
2024-03-31 12:17:10 -0400

    imping data extraction and exploration

---
+++ Preprocessing/DataHelper.py
@@ -0,0 +1,8 @@
+import numpy as np
+import pandas as pd
+
+class DataHelper:
+
+    @staticmethod
+    def get_concatinated_data(name, data):
+        pass
--- Preprocessing/ImportData.py
+++ Preprocessing/ImportData.py
@@ -1,21 +1,32 @@
 import numpy as np
 import pandas as pd
 
-class ImportData:
-
-    @staticmethod
-    def grab_all_leagues_data():
+def data_helper(name, data):
+    '''
         '''
-            Parses through the given datapath for the data in .csv format.
-            Returns a dataframe that contains all data from all leagues for a given 
-            'GET' item and 'PAY' item. 
-            '''
-        leagues = ['Betrayal', 'Breach', 'Delirium', 'Delve', 'Heist', 'Legacy',
-                   'Abyss', 'Bestiary', 'Essence', 'Harbinger', 'Metamorph',
-                   'Synthesis', 'Incursion', 'Legion', 'Ritual', 'Blight',
-                   'Harvest', 'Ultimatum', 'Expedition', 'Scourge', 'Archnemesis',
-                   'Sentinel', 'Kalandra', 'Sanctum', 'Crucible', 'Ancestor']
-        
-        print(len(leagues))
 
-ImportData.grab_all_leagues_data()
+def grab_all_leagues_data():
+    '''
+        Parses through the given datapath for the data in .csv format.
+        Returns a dataframe that contains all data from all leagues for a given 
+        'GET' item and 'PAY' item. 
+        '''
+    leagues = ['Betrayal', 'Breach', 'Delirium', 'Delve', 'Heist', 'Legacy',
+                'Abyss', 'Bestiary', 'Essence', 'Harbinger', 'Metamorph',
+                'Synthesis', 'Incursion', 'Legion', 'Ritual', 'Blight',
+                'Harvest', 'Ultimatum', 'Expedition', 'Scourge', 'Archnemesis',
+                'Sentinel', 'Kalandra', 'Sanctum', 'Crucible', 'Ancestor']
+    
+    # Load all data for currency in each league
+    df_list = list()
+    for league in leagues:
+        file_path = "data/" + league + "/" + league + ".currency.csv"
+        print(f"Loading {league} currency item data")
+
+        loaded_df = pd.read_csv(file_path)
+        df_list.append(loaded_df)
+
+    # Combine seperate league data into one dataframe and save for later use
+    unfiltered_df = pd.concat(df_list)
+    unfiltered_df.to_csv('data/currency.csv', index=False)
+
