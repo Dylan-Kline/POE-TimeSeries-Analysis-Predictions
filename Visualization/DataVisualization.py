58384c94fc4a236b45759b847c8502703f8200cd
Dylan-Kline <klinedylan46@gmail.com>
2024-03-31 14:07:48 -0400

    imp'd all values visualization

--- Visualization/DataVisualization.py
+++ Visualization/DataVisualization.py
@@ -8,3 +8,19 @@ class DataVisualization:
     @staticmethod
     def histodist_for_features(data):
         pass
+
+    @staticmethod
+    def visualize_price_all_leagues(data):
+        '''
+            Visualizes the price for all leagues in one graph.
+            '''
+        # Create figure and axis
+        figure = plt.figure(figsize=(15, 10))
+        ax = figure.add_subplot(1, 1, 1)
+
+        # Convert 'Date' column in dataframe to datetime and plot using seaborn
+        data['Date'] = pd.to_datetime(data['Date'])
+        sb.lineplot(data=data, x='Date', y='Value', hue='League', ax=ax)
+
+        plt.tight_layout()
+        plt.show()
--- main.py
+++ main.py
@@ -1,14 +1,10 @@
 import numpy as np
 import pandas as pd
 from Preprocessing.DataHelper import *
+from Visualization.DataVisualization import *
 
 def main():
     df = pd.read_csv("data/unengineered_exalt_currency.csv")
-    df['Get'] = df['Get'].replace('Divine Orb', 'Exalted Orb')
-    df = df.loc[:, ~df.columns.str.contains('^Unnamed')]
-    df.to_csv('data/unengineered_exalt_currency.csv', index=False)
-    print(df)
-    
-
+    print(df.columns)
 
 main()
